e6e7a8886f08fb4d2c2ab35c06da0ac59f723411
Dmitriy <dnagovicin@gmail.com>
2018-09-20 16:25:30 +0500

    Add c# test files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()
+        {
+
+            bool f1 = true;
+            bool f2 = false;
+            float a = 12.45f;
+
+            Console.WriteLine(f1);
+            Console.WriteLine(!f1);
+        Console.WriteLine("логическое И " + (f1 && f2));
+        Console.WriteLine("логическое ИЛИ " + (f1 || f2));
+        Console.WriteLine("логическое исключающее ИЛИ " + (f1 ^ f2));
+        Console.WriteLine(12345.456.ToString("# ## ##.##"));
+        //PrintInfo();
+
+        Console.ReadKey();
+
+        }
+
+    private static void PrintInfo()
+    {
+        Console.WriteLine("Сколько конфет у Ивана");
+        int user1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Сколько конфет у Петра");
+        int user2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Всего конфет " + (user1 + user2));
+        Console.ReadKey();
+        
+    }
+}
